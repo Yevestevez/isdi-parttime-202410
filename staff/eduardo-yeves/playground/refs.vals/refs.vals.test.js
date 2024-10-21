b89082b7d792fa0beb169bda28ae1c231b8b3e31
Yevestevez <yevestevez.cg@gmail.com>
2024-10-21 09:09:27 +0200

    add ref.vals test #5

---
+++ staff/eduardo-yeves/playground/refs.vals/refs.vals.test.js
@@ -0,0 +1,34 @@
+var edu = {
+    name: 'Eduardo',
+    lastName: 'Yeves',
+    age: 34,
+    partner: 'Ana',
+    mother: 'Loli',
+    father: 'Eduardo',
+    siblings: ['Patri', 'David'],
+    dog: 'Draco',
+}
+
+var ana = {
+    name: 'Ana',
+    lastName: 'Pérez',
+    age: 34,
+    partner: edu,
+    mother: 'Teresa',
+    brother: 'Manu',
+    Dog: 'Draco'
+}
+
+var draco = {
+    name: 'Draco',
+    age: 5,
+    guardians: [edu, ana]
+}
+
+var favoriteFood = 'Piza'
+
+var family = {
+    members: [ana, edu, draco],
+    address: 'Madrid',
+    fridayDinner: favoriteFood
+}
