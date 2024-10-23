8427d7c4b4be028b7add37ed70d3aaf89b52cf11
Yevestevez <yevestevez.cg@gmail.com>
2024-10-23 12:14:02 +0200

    add dom folder with index.html and main.js #5

---
+++ staff/eduardo-yeves/playground/dom/main.js
@@ -0,0 +1,42 @@
+console.log('Hello DOM!')
+
+function printDOMTree() {
+    //TODO -> construir una utilidad que nos permita extraer el árbol de nuestra página (HINT use recursion)
+}
+
+printDOMTree()
+
+/*
+html
+    head
+        meta
+        meta
+        title
+    body
+        h1
+        p
+        h2
+        ul
+            li
+            li
+            li
+        h2
+        table
+            tr
+                th
+                th
+                th
+            tr
+                td
+                td
+                td
+            tr
+                td
+                td
+                td
+            tr
+                td
+                td
+                td
+        script
+*/
