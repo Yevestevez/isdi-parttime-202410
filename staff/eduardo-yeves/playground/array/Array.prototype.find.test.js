2f244adf2ef72031a8bd749340a1966d77a94f50
Yevestevez <yevestevez.cg@gmail.com>
2024-10-29 14:00:34 +0100

    add Array & Dorraymon .prototype.find.test.js #5

---
+++ staff/eduardo-yeves/playground/array/Array.prototype.find.test.js
@@ -0,0 +1,39 @@
+console.log('TEST Array.prototype.find')
+
+console.log('CASE find sponge bobo in characters')
+
+var characters = [
+    { name: 'Mickey Mouse', icon: '🐭' },
+    { name: 'Tweety', icon: '🐥' },
+    { name: 'Sponge Bob', icon: '🧽' },
+    { name: 'Coyote', icon: '🦊' }
+]
+
+var character = characters.find(function (character) {
+    // if (character.name === 'Sponge Bob')
+    //  return true
+
+    // return false
+
+    return character.name === 'Sponge Bob'
+})
+
+console.log(character)
+// { name: 'Sponge Bob', icon: '🧽' }
+
+
+console.log('CASE find pink panther in characters')
+
+var characters = [
+    { name: 'Mickey Mouse', icon: '🐭' },
+    { name: 'Tweety', icon: '🐥' },
+    { name: 'Sponge Bob', icon: '🧽' },
+    { name: 'Coyote', icon: '🦊' }
+]
+
+var character = characters.find(function (character) {
+    return character.name === 'Pink Panther'
+})
+
+console.log(character)
+// undefined
