9c58254bf5d19f2596cffce4db28daefcc9ef30a
Yevestevez <yevestevez.cg@gmail.com>
2025-02-19 08:11:04 +0100

    add getUserName and createPost spec tests; implemente error handlers and express routers in api #25

---
+++ staff/eduardo-yeves/product/api/routes/posts/handlers/index.js
@@ -0,0 +1,13 @@
+import createPostHandler from './createPostHandler.js';
+import deletePostHandler from './deletePostHandler.js';
+import getPostsHandler from './getPostsHandler.js';
+import toggleLikePostHandler from './toggleLikePostHandler.js';
+import updatePostTextHandler from './updatePostTextHandler.js';
+
+export {
+    createPostHandler,
+    deletePostHandler,
+    getPostsHandler,
+    toggleLikePostHandler,
+    updatePostTextHandler
+}
